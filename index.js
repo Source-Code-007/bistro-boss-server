67f8179c953b0d0ed77de646e7edaf04878eb33a
Source-Code-007 <utsho926@gmail.com>
2023-05-30 10:53:02 +0600

    increase quantity for duplicated item added in cart

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const app = express()
 require("dotenv").config();
@@ -9,8 +9,8 @@ const port = process.env.PORT || 2500
 app.use(cors())
 app.use(express.json())
 
-app.get(('/'), (req, res)=>{
-    res.send('bistro boss server is perfectly running')
+app.get(('/'), (req, res) => {
+  res.send('bistro boss server is perfectly running')
 })
 
 
@@ -35,11 +35,58 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
     const bistroBossDB = client.db('bistro-boss')
     const menuCollection = bistroBossDB.collection('menu-collection')
+    const cartItemCollection = bistroBossDB.collection('cart-item-collection')
 
-    app.get('/menu-collection', async (req, res)=> {
-        const result = await menuCollection.find({}).toArray()
-        res.send(result)
+    // Get all menu
+    app.get('/menu-collection', async (req, res) => {
+      const result = await menuCollection.find({}).toArray()
+      res.send(result)
     })
+
+    // get cart item for specific user via email
+    app.get('/cart-item', async (req, res) => {
+      if (!req.query?.email) {
+        return res.send([])
+      }
+      const find = { email: req.query.email }
+      const result = await cartItemCollection.find(find).toArray()
+      res.send(result)
+    })
+
+    // add to cart route
+    app.post('/cart-item', async (req, res) => {
+      const item = req.body
+      item.itemId = item._id
+      delete item._id
+
+      const find = {email: req.query?.email, itemId: item.itemId}
+      const existingItem = await cartItemCollection.findOne(find)
+
+      if(existingItem){
+        const options = {upsert: true}
+        const updatedItem = {
+          $set: {
+            ...existingItem, quantity: existingItem.quantity+=1
+          },
+        };
+        const result = await cartItemCollection.updateOne(find, updatedItem, options)
+        res.send(result)
+      } else{
+        item.quantity = 1
+        const result = await cartItemCollection.insertOne(item)
+       res.send(result)
+      }
+    })
+
+    // delete item from cart
+    app.delete(`/cart-item/:id`, async (req, res) => {
+      const uniqueId = req.params.id
+      const find = {_id: new ObjectId(uniqueId)}
+      const result = await cartItemCollection.deleteOne(find)
+      res.send(result)
+    })
+
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -48,6 +95,6 @@ async function run() {
 run().catch(console.dir);
 
 
-app.listen(port, ()=>{
-    console.log('bistro boss server is running successfully!');
+app.listen(port, () => {
+  console.log('bistro boss server is running successfully!');
 })
